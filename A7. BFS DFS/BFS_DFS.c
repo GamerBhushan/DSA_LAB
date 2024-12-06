77adfb8214960f1cef9ac99a7ad30043190dac27
Bhushan <kumavatbhushansanjay@gmail.com>
2024-12-06 21:54:53 +0530

    Bhushan

---
+++ A7. BFS DFS/BFS_DFS.c
@@ -0,0 +1,24 @@
+/*
+WAP To Create A Graph use the map of your city as the graph
+using adjacency list / adjacency matrix and perform BFS and DFS
+
+*/
+
+// In BFS We Use Queue FIFO.
+// In DFS We Use Statck LIFO.
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct gp
+{
+    char city[100];
+    struct gp *left,*right;
+}Graph;
+
+
+
+int main(int argc, char const *argv[]){
+    
+    return 0;
+}
