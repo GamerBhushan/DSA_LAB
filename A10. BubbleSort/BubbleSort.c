47f6b1b31a5f50d4036259928db71478bc6702c6
Bhushan <kumavatbhushansanjay@gmail.com>
2024-12-19 19:33:30 +0530

    Bhushan

---
+++ A10. BubbleSort/BubbleSort.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+
+#ifndef BUBBLESORT_H
+#define BUBBLESORT_H
+
+void BubbleSort(int size, int data[], int type);
+
+#endif // BUBBLESORT_H
+
+
+
+
+
+int generateRandomInt(int min, int max)
+{
+    return (rand() % (max - min + 1)) + min; // Generate random number in range
+}
+
+void BubbleSort(int size, int data[], int type) {
+    if (type == 0) { // Sorting in Ascending Order
+        for (int i = 0; i < size - 1; i++) {
+            int swapped = 0; // Optimization: Check if any swapping occurred
+            for (int j = 0; j < size - i - 1; j++) { // Only go up to unsorted portion
+                if (data[j] > data[j + 1]) {
+                    int tmp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = tmp;
+                    swapped = 1; // Mark that a swap occurred
+                }
+            }
+            if (!swapped) break; // Exit early if no swaps happened
+        }
+    } else if (type == 1) { // Sorting in Descending Order
+        for (int i = 0; i < size - 1; i++) {
+            int swapped = 0; // Optimization: Check if any swapping occurred
+            for (int j = 0; j < size - i - 1; j++) {
+                if (data[j] < data[j + 1]) {
+                    int tmp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = tmp;
+                    swapped = 1; // Mark that a swap occurred
+                }
+            }
+            if (!swapped) break; // Exit early if no swaps happened
+        }
+    } else {
+        printf("\n\nUndefined Type: Choose '0' for Ascending or '1' for Descending\n\n");
+    }
+}
+
+void display(int size, int data[])
+{
+    for (int i = 0; i < 20; i++)
+    {
+        if (i == 19)
+        {
+            printf(" %d ", data[i]);
+        }
+        else
+        {
+            printf(" %d, ", data[i]);
+        }
+    }
+}
+
+void changeValue(int data[]){
+    data[0] = 0;
+    data[1] = 0;
+}
---
+++ A10. BubbleSort/StudentBubbleSort.c
@@ -0,0 +1,14 @@
+/*
+
+Write a program to arrange list of students to find out first ten toppers from a class using 
+Bubble sort
+
+*/
+
+#include<stdio.h>
+#include "BubbleSort.c"
+
+int main(int argc, char const *argv[]){
+    
+    return 0;
+}
