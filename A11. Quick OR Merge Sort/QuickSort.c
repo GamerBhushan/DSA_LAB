1c88dbd160112b336cb662edc497de90f4dea9c7
Bhushan <kumavatbhushansanjay@gmail.com>
2024-12-25 11:05:51 +0530

    Bhushan

--- A11. Quick OR Merge Sort/QuickSort.c
+++ A11. Quick OR Merge Sort/QuickSort.c
@@ -1,18 +1,45 @@
-#include<stdio.h>
-#include"SalesDatabase.c"
+#include <stdio.h>
+#include "SalesDatabase.c"
 
-void QuickSort(Sales sales[]){
-    int pivot = MAX_SALES / 2; // Considering The Middle Is Pivot
-    
 
+void swapSales(int a, int b)
+{
+    Sales temp;
+    temp = sales[a];
+    sales[a] = sales[b];
+    sales[b] = temp;
 }
 
-void Partioning
+// Partition function
+int Partition(Sales sales[], int start, int end) {
+    double pivot = sales[end].total_sale_amount; // Choose last element as pivot
+    int i = start - 1; // Partition index
+
+    for (int j = start; j < end; j++) {
+        if (sales[j].total_sale_amount <= pivot) {
+            i++;
+            swapSales(i, j);
+        }
+    }
+
+    swapSales(i + 1, end); // Place pivot in correct position
+    return i + 1;
+}
+
+// QuickSort function
+void QuickSort(Sales sales[], int start, int end) {
+    if (start < end) {
+        int pivotIndex = Partition(sales, start, end);
+        QuickSort(sales, start, pivotIndex - 1); // Sort left partition
+        QuickSort(sales, pivotIndex + 1, end);   // Sort right partition
+    }
+}
 
 
-int main(int argc, char const *argv[])
-{
-    initSalesDatabase();
+int main(int argc, char const *argv[]){
+    displaySales();
+    printf("\n--------------------------------- AFTER SORTING IN DESCENDING ORDER ----------------------------\n");
+    QuickSort(sales,0,MAX_SALES-1);
     displaySales();
     return 0;
 }
--- A11. Quick OR Merge Sort/SalesDatabase.c
+++ A11. Quick OR Merge Sort/SalesDatabase.c
@@ -1,60 +1,54 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define MAX_SALES 5
 
-typedef struct Sales{
+typedef struct Sales
+{
     int sales_id;
     char *customer_name;
     char *product_name;
     int quantity_sold;
     double total_sale_amount;
-}Sales;
+} Sales;
 
-
-int generateRandomInt(int min, int max){
+int generateRandomInt(int min, int max)
+{
     return (rand() % (max - min + 1)) + min; // Generate random number in range
 }
 
-double generateRandomDouble(double min, double max) {
+double generateRandomDouble(double min, double max)
+{
     double random = (double)rand() / RAND_MAX; // Random value between 0.0 and 1.0
-    return min + random * (max - min);        // Scale to the range [min, max]
+    return min + random * (max - min);         // Scale to the range [min, max]
 }
 
-Sales sales[MAX_SALES];
+Sales sales[MAX_SALES] = {
+    {1, "Bhushan", "Chocklet Box", 2, 150},
+    {2, "Vijay", "Chocklet Box", 8, 100},
+    {3, "Anushka", "Chocklet Box", 5, 75},
+    {4, "Ajay", "Chocklet Box", 1, 95},
+    {5, "Deepika", "Chocklet Box", 10, 175}};
 
-void initSalesDatabase(){
-    double iphonePrice = 10000.00;
-    char *names[] = {"Bhushan","Vijay","Anushka","Ajay","Deepika"};
-    for (int  i = 0; i < MAX_SALES; i++){
-        sales[i].sales_id = i+1;
-        sales[i].customer_name = names[i];
-        sales[i].product_name = "iPhone";
-        sales[i].quantity_sold = generateRandomInt(1,10);
-        sales[i].total_sale_amount = sales[i].quantity_sold * iphonePrice;
-    }
-}
-
-void displaySales(){
+void displaySales()
+{
     for (int i = 0; i < MAX_SALES; i++)
     {
-        printf("\nID : %d",sales[i].sales_id);
-        printf("\nCustomer Name : %s",sales[i].customer_name);
-        printf("\nProduct Name : %s",sales[i].product_name);
-        printf("\nQuantity Sold : %d",sales[i].quantity_sold);
-        printf("\nTotal Sale Amount : %.2f",sales[i].total_sale_amount);
+        printf("\nID : %d", sales[i].sales_id);
+        printf("\nCustomer Name : %s", sales[i].customer_name);
+        printf("\nProduct Name : %s", sales[i].product_name);
+        printf("\nQuantity Sold : %d", sales[i].quantity_sold);
+        printf("\nTotal Sale Amount : %.2f", sales[i].total_sale_amount);
         printf("\n\n");
     }
-    
 }
 
 // int main(int argc, char const *argv[])
 // {
 //     initSalesDatabase();
 //     displaySales();
-    
+
 //     return 0;
 // }
-
