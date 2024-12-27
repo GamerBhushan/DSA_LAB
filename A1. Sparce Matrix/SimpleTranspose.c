bf30391b769f49d9b19edbec91d344562892a708
Bhushan <kumavatbhushansanjay@gmail.com>
2024-12-27 14:50:28 +0530

    Bhushan

--- A1. Sparce Matrix/SimpleTranspose.c
+++ A1. Sparce Matrix/SimpleTranspose.c
@@ -5,12 +5,12 @@
 
 void displayMatrix(char message[], int row, int col, int matrix[row][col]);
 void createSparceMatrix(int row, int col, int totalNonZeroValues, int matrix[row][col], int sparce[totalNonZeroValues + 1][3]);
-void fastTranspose(int row, int col, int sparce_max_row, int sparce_max_col, int matrix[row][col], int sparce[sparce_max_col][sparce_max_row]);
+void simpleTranspose(int row, int col, int sparce_max_row, int sparce_max_col, int matrix[row][col], int sparce[sparce_max_col][sparce_max_row]);
 void display1DArray(char message[], int size, int arr[size]);
 
 void display1DArray(char message[], int size, int arr[size])
 {
-    printf("%s",message);
+    printf("%s", message);
     for (int i = 0; i < size; i++)
     {
         printf("%d   ", arr[i]);
@@ -53,47 +53,21 @@ void createSparceMatrix(int row, int col, int totalNonZeroValues, int matrix[row
     }
 }
 
-void fastTranspose(int row, int col, int sparce_max_row, int sparce_max_col, int matrix[row][col], int sparce[sparce_max_col][sparce_max_row])
+void simpleTranspose(int row, int col, int sparce_max_row, int sparce_max_col, int matrix[row][col], int sparce[sparce_max_col][sparce_max_row])
 {
-    int total[col];
-
-    for (int i = 0; i < col; i++){
-        total[i] = 0;
-    }
-    
-    for (int i = 1; i <= sparce_max_row; i++){
-        int col_no = sparce[i][1];
-        total[col_no]++;
-    }
-    
-
-    int index[col + 1];
-    index[0] = 1; // By Rule
-
-    for (int i = 1; i <= col + 1; i++)
-    {
-        index[i] = index[i - 1] + total[i - 1]; // Formula
-    }
-
-    display1DArray("\nThe Total Array Is :  ",col,total);
-    display1DArray("\nThe Index Array Is :  ",col+1,index);
-
     int transpose[sparce_max_row][sparce_max_col];
-
     transpose[0][0] = sparce[0][1];
     transpose[0][1] = sparce[0][0];
     transpose[0][2] = sparce[0][2];
 
-    for (int i = 1; i <= sparce[0][2]; i++){
-        int rowIndex = sparce[i][1];
-        int placedAtRow = index[rowIndex];
-        transpose[placedAtRow][0] = sparce[i][1];
-        transpose[placedAtRow][1] = sparce[i][0];
-        transpose[placedAtRow][2] = sparce[i][2];
-        index[rowIndex]++;
-    }
-    displayMatrix("\n\nFast Transpose : \nRow\tColumn\tValue\n",sparce_max_row,sparce_max_col,transpose);
+    for (int i = 1; i <= sparce[0][2]; i++)
+    {
 
+        transpose[i][0] = sparce[i][1];
+        transpose[i][1] = sparce[i][0];
+        transpose[i][2] = sparce[i][2];
+    }
+    displayMatrix("\nSimple Transpose Is \nRow\tColumn\tValue", sparce_max_row, sparce_max_col, transpose);
 }
 
 int main()
@@ -114,6 +88,9 @@ int main()
             }
         }
     }
+
+    printf("Non Zero : %d",total_non_zero );
+
     int sparce_max_row = total_non_zero + 1, sparce_max_col = 3;
     int sparce[sparce_max_row][sparce_max_col];
 
@@ -121,5 +98,5 @@ int main()
     createSparceMatrix(max_row, max_col, total_non_zero, matrix, sparce);
     displayMatrix("Sparce Matrix Representation : \nRow\tColumn\tValue\n", sparce_max_row, sparce_max_col, sparce);
 
-    fastTranspose(max_row, max_col, sparce_max_row, sparce_max_col, matrix, sparce);
+    simpleTranspose(max_row, max_col, sparce_max_row, sparce_max_col, matrix, sparce);
 }
